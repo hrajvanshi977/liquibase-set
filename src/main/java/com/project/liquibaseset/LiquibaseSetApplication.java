2ec65dedc155d8fc09cc23a93db03dae078056df
Himanshu Rajvanshi <himanshu.rajvanshi@synoriq.in>
2021-12-06 18:29:00 +0530

    Initial commit

---
+++ src/main/java/com/project/liquibaseset/LiquibaseSetApplication.java
@@ -0,0 +1,12 @@
+package com.project.liquibaseset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseSetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseSetApplication.class, args);
+	}
+}
---
+++ src/test/java/com/project/liquibaseset/LiquibaseSetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.liquibaseset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseSetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
